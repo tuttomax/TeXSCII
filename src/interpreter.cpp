87566cdaae69b0fb07e87c3a170c7171c2fd5aa4
tuttomax <tuttomax@users.noreply.github.com>
2017-08-31 21:52:11 +0200

    changes

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -3,69 +3,22 @@
 glyph_container interpreter::run(token_container &tokens)
 {
     glyph_container container;
+    
+    token_container previous_backtrack;
 
     while (!tokens.empty())
     {
-        token current_token = tokens.front();
+        previous_backtrack.push(tokens.top());
         tokens.pop();
-        token next_token = tokens.front();
-
+        
+        token next_token = tokens.top();
+        tokens.pop();
+        
         if (next_token.type == sup)
         {
-            token.pop();
-            
-            std::stack<token> backtrack_bracket;
-            token_container backtrack;
 
-            auto sup_ptr = std::make_unique<sup_glyph>(current_token.identifier);
+        }    
 
-            while (current_token.type != close_b || !backtrack_bracket.empty())
-            {
-                current_token = tokens.front();
-                tokens.pop();
-
-                if (current_token.type == open_b)
-                    backtrack_bracket.push(current_token);
-                if (current_token.type == close_b)
-                    backtrack_bracket.pop();
-
-                backtrack.push(current_token);
-            }
-
-            sup_ptr->sup = run(backtrack);
-
-            container.push(std::move(sup_ptr));
-        }
-        if (current_token.type == sub)
-        {
-            std::stack<token> backtrack_bracket;
-            token_container backtrack;
-
-            auto sub_ptr = std::make_unique<sub_glyph>();
-
-            while (current_token.type != close_b || !backtrack_bracket.empty())
-            {
-                current_token = tokens.front();
-                tokens.pop();
-
-                if (current_token.type == open_b)
-                    backtrack_bracket.push(current_token);
-                if (current_token.type == close_b)
-                    backtrack_bracket.pop();
-
-                backtrack.push(current_token);
-            }
-
-            sub_ptr->sub = run(backtrack);
-
-            container.push(std::move(sub_ptr));
-        }
-        else if (current_token.type == general_string ||
-                 current_token.type == number ||
-                 current_token.type == symbol)
-        {
-            add_glyph(container, current_token);
-        }
     }
 
     return container;
--- src/token.hpp
+++ src/token.hpp
@@ -1,31 +1,33 @@
 #ifndef TOKEN_HPP
 #define TOKEN_HPP
 
+#include <stack>
 #include <string>
-#include <queue>
 
-enum token_type 
+
+enum token_type
 {
-    general_string,       // any name: function,variable
-    open_b,         // {
-    close_b,        // }
-    open_p,         // ( 
-    close_p,        // )
-    number,         // any digit
-    symbol,         // handle +,-,*,/,^,=
-    sub,            // handle ^ for writing on top
-    sup,            // handle _ for writing on bottom    
-   
-    builtinfunc,
+    general_string,
+    symbol,
+    number,
 
+    open_p,
+    close_p,
+    open_b,
+    close_b,
+    
+    sup,
+    sub,
+
+    builtinfunc
 };
 
 struct token
 {
     token_type type;
-    std::string identifier; // will be used by interpreter
+    std::string identifier;
 };
 
-using token_container = std::queue<token>;
+using token_container = std::stack<token>;
 
-#endif //TOKEN_HPp
+#endif //TOKEN_HPP
